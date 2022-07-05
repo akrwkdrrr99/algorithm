9bef1f85e9478529b482d2249a757e5bdae56f5e
hollosum <akrwkdrrr99@gmail.com>
2022-07-05 11:33:05 +0900

    main원상복구

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
-import java.io.*;
-import java.util.*;
 import java.util.*;
 
 public class Main {
-//    public static void main(String[] args)  {
-//
-//    }
+    public static void main(String[] args)  {
+    }
 }
