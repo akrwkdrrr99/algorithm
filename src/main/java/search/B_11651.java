ac14c6ed84237198f8b0389f74ff37848d9adec8
hollosum <akrwkdrrr99@gmail.com>
2022-07-13 05:51:24 +0900

    CompareTo, Comparator 두 개를 확인.

---
+++ src/main/java/search/B_11651.java
@@ -0,0 +1,62 @@
+package search;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class B_11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for(int i =0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+        }
+//        //람다식
+//        Arrays.sort(arr, (e1, e2) -> {
+//            if(e1[0] == e2[0]) {
+//                return e1[1] - e2[1];
+//            } else {
+//                return e1[0] - e2[0];
+//            }
+//        });
+
+        // 기본식
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                } else {
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+
+
+//        //풀었을 때
+//        for(int i= 0; i<N; i++) {
+//            if(i < N) {
+//                if(arr[i][0] == arr[i+1][0]) {
+//                    if (arr[i][1] > arr[i+1][1]) {
+//                        int k = arr[i][1];
+//                        arr[i][1] = arr[i+1][1];
+//                    }
+//                }
+//
+//            }
+//        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N; i++) {
+            sb.append(arr[i][1] + " " + arr[i][0]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
